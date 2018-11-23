0253bcb82544ab76772c0825466a60c596c781bc
andrasfelfoldi <andrasfelfoldi@gmail.com>
2018-11-23 23:30:03 +0100

    todo implement thunk

---
+++ src/reducers/movies.js
@@ -0,0 +1,12 @@
+import actions from '../constants/actionConstants';
+
+export default (state=[], action) => {
+    switch (action.type) {
+        case actions.CREATE_MOVIE:
+            //TODO implement thunk
+            break;
+    
+        default:
+            break;
+    }
+}
