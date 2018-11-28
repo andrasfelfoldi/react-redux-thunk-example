8311ec477ff92f21e5a4bcd8494bad4980879bbf
andrasfelfoldi <andrasfelfoldi@gmail.com>
2018-11-28 19:08:04 +0100

    almost finished form and fixed issue with passed in function

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
   submitForm=(event)=>{
     event.preventDefault();
-    console.log("misi")
+    console.log(this)
   }
 
   render() {
--- src/component/MovieForm.js
+++ src/component/MovieForm.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class MovieForm extends React.Component {
-    constructor(title, releaseYear, rating, onSubmitClicked){
-        super(title, releaseYear, rating, onSubmitClicked);
+
+    constructor(props) {
+        super(props);
+        if(this.props.movie){
+            this.state={title: this.props.movie.title, releaseYear: this.props.movie.releaseYear, rating: this.props.movie.rating};
+        }else{
+            this.state={title: "", releaseYear: "", rating: ""};
+        }
+    }
+
+    onTitleChanged = event => {
+        this.setState({title: event.target.value});
+    }
+
+    onReleaseYearChanged = event => {
+        this.setState({releaseYear: event.target.value});
+    }
+
+    onRatingChanged = event => {
+        this.setState({rating: event.target.value});
     }
 
     render(){
@@ -11,15 +29,19 @@ class MovieForm extends React.Component {
     <Form style={{padding: "15px"}}>
         <FormGroup>
             <Label for="title">Title</Label>
-            <Input type="text" name="title" id="title" placeholder="Enter a movie title" />
+            <Input type="text" name="title" id="title" placeholder="Enter a movie title"
+                value={this.state.title} onChange={event => this.onTitleChanged(event)}/>
         </FormGroup>
         <FormGroup>
             <Label for="releaseYear">Release Year</Label>
-            <Input type="number" name="releaseYear" id="releaseYear" placeholder="Enter a release year" />
+            <Input type="number" name="releaseYear" id="releaseYear" placeholder="Enter a release year"
+                value={this.state.releaseYear} onChange={event => this.onReleaseYearChanged(event)}/>
         </FormGroup>
         <FormGroup>
             <Label for="rating">Rating</Label>
-            <Input type="select" name="rating" id="rating">
+            <Input type="select" name="rating" id="rating"
+                value={this.state.rating} onChange={event => this.onRatingChanged(event)}>
+                <option value="">None</option>
                 <option>10</option>
                 <option>9</option>
                 <option>8</option>
@@ -32,7 +54,7 @@ class MovieForm extends React.Component {
                 <option>1</option>
             </Input>
         </FormGroup>
-        <Button color="success" style={{margin: " 0px 5px"}} onClick={(event) => console.log(this.onSubmitClicked)}>Submit</Button>
+        <Button color="success" style={{margin: " 0px 5px"}} onClick={(event) => this.props.onSubmitClicked(event)}>Submit</Button>
         <Button style={{margin: "0px 5px"}}>Cancel</Button>
     </Form>
      );
